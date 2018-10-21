bd5aeadc580642241cc5bc39e87debddcf380ced
Michael Weiss <dev.primeos@gmail.com>
2018-10-21 11:50:41 +0200

    sway: 1.0-alpha.6 -> 1.0-beta.1

--- default.nix
+++ default.nix
@@ -4,24 +4,32 @@
 self: super:
 
 {
-  meson471 = super.meson.overrideAttrs (oldAttrs: rec {
+  meson480 = super.meson.overrideAttrs (oldAttrs: rec {
     name = pname + "-" + version;
     pname = "meson";
-    version = "0.47.1";
+    version = "0.48.0";
 
     src = super.python3Packages.fetchPypi {
       inherit pname version;
-      sha256 = "090vap8bckllg4k58j65jm6472qp53l49iacy0gpcykcxirjbxwp";
+      sha256 = "0qawsm6px1vca3babnqwn0hmkzsxy4w0gi345apd2qk3v0cv7ipc";
     };
+    patches = builtins.filter # Remove gir-fallback-path.patch
+      (str: !(super.lib.hasSuffix "gir-fallback-path.patch" str))
+      oldAttrs.patches;
   });
   wlroots = super.wlroots.overrideAttrs (oldAttrs: rec {
-    name = "wlroots-unstable-2018-09-19";
+    name = "wlroots-${version}";
+    version = "0.1";
     src = super.fetchFromGitHub {
       owner = "swaywm";
       repo = "wlroots";
-      rev = "842368ec983cdf671cf16f5be51633392ed52d5e";
-      sha256 = "0cnndpv0s6502s2a2a4nm2az752fkdzcpp6v973dn69gv9rijnyp";
+      rev = version;
+      sha256 = "0xfipgg2qh2xcf3a1pzx8pyh1aqpb9rijdyi0as4s6fhgy4w269c";
     };
+    patches = [ (super.fetchpatch { # TODO: Only for version 0.1
+      url = https://github.com/swaywm/wlroots/commit/be6210cf8216c08a91e085dac0ec11d0e34fb217.patch;
+      sha256 = "0njv7mr4ark603w79cxcsln29galh87vpzsx2dzkrl1x5x4i6cj5";
+    }) ];
     # $out for the library, $bin for rootston, and $examples for the example
     # programs (in examples) AND rootston
     outputs = [ "out" "bin" "examples" ];
@@ -29,7 +37,7 @@ self: super:
       "-Dlibcap=enabled" "-Dlogind=enabled" "-Dxwayland=enabled" "-Dx11-backend=enabled"
       "-Dxcb-icccm=enabled" "-Dxcb-xkb=enabled" "-Dxcb-errors=enabled"
     ];
-    nativeBuildInputs = [ self.meson471 super.ninja super.pkgconfig ];
+    nativeBuildInputs = [ self.meson480 super.ninja super.pkgconfig ];
     buildInputs = (with super; [
       wayland libGL wayland-protocols libinput libxkbcommon
       pixman libcap mesa_noglu
@@ -66,17 +74,13 @@ self: super:
   });
   sway = super.sway.overrideAttrs (oldAttrs: rec {
     name = "sway-${version}";
-    version = "1.0-alpha.6";
+    version = "1.0-beta.1";
     src = super.fetchFromGitHub {
       owner = "swaywm";
       repo = "sway";
       rev = version;
-      sha256 = "0358c4ga8jm45apqck1y3hqws4qxnsf8z8r881q905b9g54nli8k";
+      sha256 = "0h9kgrg9mh2acks63z72bw3lwff32pf2nb4i7i5xhd9i6l4gfnqa";
     };
-    postPatch = ''
-      substituteInPlace meson.build --replace \
-        "werror=true" "werror=false"
-    '';
     mesonFlags = [ "-Dsway-version=${version}" ];
     nativeBuildInputs = with super; [
       meson pkgconfig ninja
