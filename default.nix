094ee84700f50002c82e1d718d19885ac4129925
Michael Weiss <dev.primeos@gmail.com>
2019-05-12 12:03:15 +0200

    wlroots: 0.5.0 -> 0.6.0

--- default.nix
+++ default.nix
@@ -4,4 +4,17 @@
 self: super:
 
 {
+  wlroots = super.wlroots.overrideAttrs (oldAttrs: rec {
+    name = "wlroots-${version}";
+    version = "0.6.0";
+    src = super.fetchFromGitHub {
+      owner = "swaywm";
+      repo = "wlroots";
+      rev = version;
+      sha256 = "1rdcmll5b8w242n6yfjpsaprq280ck2jmbz46dxndhignxgda7k4";
+    };
+    buildInputs = oldAttrs.buildInputs ++ [ super.freerdp ];
+    mesonFlags = oldAttrs.mesonFlags ++ [ "-Dfreerdp=enabled" ];
+    postPatch = "";
+  });
 }
