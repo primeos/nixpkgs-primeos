a15682fb12337b123d35159e920762e462b14099
Michael Weiss <dev.primeos@gmail.com>
2019-08-10 12:47:11 +0200

    sway: 1.1.1 -> 1.2-rc1

--- default.nix
+++ default.nix
@@ -4,4 +4,17 @@
 self: super:
 
 {
+  sway = super.sway.overrideAttrs (oldAttrs: rec {
+    name = "sway-${version}";
+    version = "1.2-rc1";
+    src = super.fetchFromGitHub {
+      owner = "swaywm";
+      repo = "sway";
+      rev = version;
+      sha256 = "1f2k39c4kbwxxy65qbz85bm1pf9nmgn7az90qjds9hi3m1ff5q4y";
+    };
+    postPatch = ''
+      sed -iE "s/version: '1.1'/version: '${version}'/" meson.build
+    '';
+  });
 }
