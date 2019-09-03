1daf8e580135e1af56e42f0d84d9dc8608726459
Michael Weiss <dev.primeos@gmail.com>
2019-09-03 12:13:53 +0200

    Remove Sway as version 1.2 is now in nixos-unstable

--- default.nix
+++ default.nix
@@ -4,17 +4,4 @@
 self: super:
 
 {
-  sway = super.sway.overrideAttrs (oldAttrs: rec {
-    name = "sway-${version}";
-    version = "1.2-rc2";
-    src = super.fetchFromGitHub {
-      owner = "swaywm";
-      repo = "sway";
-      rev = version;
-      sha256 = "0bqw246iyrpir2hl3sl6k2wkkzi0cypslsla59a77s9s7gnzymdw";
-    };
-    postPatch = ''
-      sed -iE "s/version: '1.1'/version: '${version}'/" meson.build
-    '';
-  });
 }
